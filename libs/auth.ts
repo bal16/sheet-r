dc569ed9804ef241050725dcc31a383adc5539da
bal16 <ahmadhamidbalya@gmail.com>
2025-12-17 09:18:36 +0700

    feat: implement authentication

---
+++ libs/auth.ts
@@ -0,0 +1,27 @@
+import { AuthOptions, getServerSession } from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+
+
+const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+}
+
+/**
+ * Helper function to get the session on the server without having to import the authOptions object every single time
+ * @returns The session object or null
+ */
+const getSession = () => getServerSession(authOptions)
+
+export { authOptions, getSession }
