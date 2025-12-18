93c126dbebd1de79fb8baacf41c48794540da8a5
bal16 <ahmadhamidbalya@gmail.com>
2025-12-18 13:57:01 +0700

    style: remove space

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,10 @@
-// lib/auth.ts
 import { betterAuth } from "better-auth";
 
 export const auth = betterAuth({
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
-   
-    // Opsi tambahan: Membatasi siapa yang boleh login (Opsional)
-    // Tapi untuk MVP, kita biarkan login dulu, baru blokir di halaman Dashboard.
+  },
 });
